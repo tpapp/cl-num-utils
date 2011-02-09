0d9e4c710ed53a14a89bba35c92c379c7ae1036e
tpapp <tkpapp@gmail.com>
2011-02-09 18:47:18 +0100

    Added IA*, a generalized case of IA.  Useful for testing displaced-subarray.

--- tests/setup.lisp
+++ tests/setup.lisp
@@ -12,11 +12,17 @@
 
 ;;; utilities
 
-(defun ia (&rest dimensions)
-  "Return an array with given dimensions, filled with integers from 0,
+(defun ia* (start &rest dimensions)
+  "Return an array with given dimensions, filled with integers from START,
 in row-major order.  For testing purposes."
   (aprog1 (make-array dimensions)
     (iter
       (for i :from 0 :below (array-total-size it))
-      (setf (row-major-aref it i) i))))
+      (for value :from start)
+      (setf (row-major-aref it i) value))))
+
+(defun ia (&rest dimensions)
+  "Return an array with given dimensions, filled with integers from 0,
+in row-major order.  For testing purposes."
+  (apply #'ia* 0 dimensions))
 
