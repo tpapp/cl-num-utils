38bc535828125b4e07f6ebd481249c26dd702bb5
tpapp <tkpapp@gmail.com>
2011-02-12 18:38:12 +0100

    Added binned-data classes and functions.

--- src/bins.lisp
+++ src/bins.lisp
@@ -81,3 +81,93 @@ accordingly, otherwise an error is signalled."
              (error "~A is above ~A, the last break."
                     value right-boundary)))
         (t (find-bin% value breaks right))))))
+
+;;; binned-data
+;;; 
+;;; Data structures for handling data that has been mapped to bins.  Convenience
+;;; functions for recovering where the data have been from (if possible), and
+;;; for the number of bins.  A vector of positive fixnums (not checked) can
+;;; also act as binned-data.
+
+(defclass binned-data ()
+  ((indexes :accessor indexes :initarg :indexes)))
+
+(defmethod indexes ((vector vector))
+  vector)
+
+(defgeneric bin-limit (binned-data)
+  (:documentation "Return an integer which larger than all indexes (but does not
+  have to be the smallest of such values).")
+  (:method ((vector vector))
+    (1+ (reduce #'max vector)))
+  (:method ((binned-data binned-data))
+    (bin-limit (indexes binned-data))))
+
+(defgeneric bin-origin (binned-data bin-index)
+  (:documentation "Return information on the particular bin (what value/range is
+  mapped to this bin) if available.")
+  (:method ((binned-data binned-data) bin-index)
+    bin-index)
+  (:method ((binned-data binned-data) bin-index)
+    bin-index))
+
+(defclass continuous-binned-data (binned-data)
+  ((breaks :accessor breaks :initarg :breaks))
+  (:documentation "Used for binning real numbers."))
+
+(defmethod bin-limit ((binned-data continuous-binned-data))
+  (length (breaks binned-data)))
+
+(defmethod bin-origin ((binned-data continuous-binned-data) bin-index)
+  (bind (((:slots-r/o breaks) binned-data))
+    (make-interval (aref breaks bin-index)
+                   (let ((right-index (1+ bin-index)))
+                     (when (< right-index (length breaks))
+                       (aref breaks right-index))))))
+
+(defun bin-using-breaks (vector breaks &key (below 0)
+                         (above (- (length breaks) 2)) copy? skip-check?)
+  (make-instance 'continuous-binned-data
+                 :indexes (map 'simple-fixnum-vector
+                               (irregular-bins breaks :below below :above above
+                                               :copy? copy?
+                                               :skip-check? skip-check?)
+                               vector)
+                 :breaks breaks))
+
+(defun bin-using-quantiles (vector quantiles)
+  "Bin VECTOR using its quantiles.  Quantiles has to contain 0 and 1.  Highest
+element is put in the last bin."
+  (assert (and (vector-satisfies? quantiles #'<)
+               (= (aref quantiles 0) 0)
+               (= (vector-last quantiles) 1)))
+  (bin-using-breaks vector (sample-quantiles vector quantiles)
+                    :skip-check? t))
+
+(defclass discrete-binned-data (binned-data)
+  ((keys :accessor keys :initarg :keys)))
+
+(defmethod bin-limit ((binned-data discrete-binned-data))
+  (length (keys binned-data)))
+
+(defmethod bin-origin ((binned-data discrete-binned-data) bin-index)
+  (aref (keys binned-data) bin-index))
+
+(defun bin-discrete (vector &key (test #'eql))
+  "Bin discrete data, using TEST.  The implementation uses a hash-table, and
+TEST has to be acceptable to MAKE-HASH-TABLE."
+  (let ((table (make-hash-table :test test)))
+    (map nil (lambda (v)
+               (setf (gethash v table) t))
+         vector)
+    (let ((keys (sort (coerce (hash-table-keys table) 'vector) #'<)))
+      (iter
+        (for key :in-vector keys :with-index key-index)
+        (setf (gethash key table) key-index))
+      (make-instance 'discrete-binned-data
+                     :indexes (map 'vector (lambda (v) (gethash v table)) vector)
+                     :keys keys))))
+
+;; (bin-using-quantiles (numseq 0 10 :length 20) (numseq 0 1 :by 1/4))
+
+;; (bin-discrete #(1 1 2 4 7))
