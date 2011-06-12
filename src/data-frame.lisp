1dda92cf4e06af72716995a8164d01d64e01be58
tpapp <tkpapp@gmail.com>
2011-06-12 14:06:45 +0200

    Commented out most of the code, the interface remains.

--- src/data-frame.lisp
+++ src/data-frame.lisp
@@ -2,95 +2,82 @@
 
 (in-package #:cl-num-utils)
 
-(defstruct (data-frame (:constructor make-data-frame% (matrix column-index)))
-  matrix
-  column-index)
+(defclass data-frame ()
+  ((elements :accessor elements :initarg :elements)
+   (layout :accessor layout :initarg :layout)))
 
-(defmethod embedded-ix ((data-frame data-frame))
-  (data-frame-column-index data-frame))
+;; (defmacro define-column-index-function (function &optional args)
+;;  `(defmethod ,function ((data-frame data-frame) ,@args)
+;;      (,function (data-frame-column-index data-frame) ,@args)))
 
-(defmacro define-column-index-function (function &optional args)
-  `(defmethod ,function ((data-frame data-frame) ,@args)
-     (,function (data-frame-column-index data-frame) ,@args)))
+(defmethod as-array ((data-frame data-frame) &key copy?)
+  (maybe-copy-array (elements data-frame) copy?))
 
-(defgeneric make-data-frame (matrix column-index &key copy?)
-  (:documentation "Create a data frame using MATRIX and COLUMN-INDEX (the latter
-  may be used to create an index, if it isn't one).  COPY? forces copying of the
-  MATRIX, otherwise may share structure."))
+(defun make-data-frame (elements layout &key copy?)
+  "Create a data frame using ELEMENTS (a matrix) and a LAYOUT.  COPY? forces
+  copying of the MATRIX, otherwise may share structure."
+  (let+ (((nil ncol) (array-dimensions elements)))
+    (assert (= (layout-length layout) ncol))
+    (make-instance 'data-frame
+                   :elements (maybe-copy-array elements copy?)
+                   :layout layout)))
 
-(defmethod make-data-frame (matrix column-index &key copy?)
-  (assert (= (array-rank matrix) 2))
-  (let ((ncol (array-dimension matrix 1)))
-    (assert (and (= (ix-start column-index) 0)
-                 (= (ix-end column-index)) ncol))
-    (make-data-frame% (maybe-copy-array matrix copy?)
-                      column-index)))
+;; (defmacro data-frame-with-resolved-index-specification
+;;     ((((matrix column-index) data-frame)
+;;       ((is0 is1) index-specifications))
+;;      &body body)
+;;   "Wrapper macro for resolving column index specifications."
+;;   (check-type matrix symbol)
+;;   (check-type column-index symbol)
+;;   (check-type is0 symbol)
+;;   (check-type is1 symbol)
+;;   (once-only (data-frame)
+;;     `(let+ (((&structure data-frame- (,matrix matrix)
+;;                          (,column-index column-index)) data-frame)
+;;             ((,is0 ,is1) ,index-specifications)
+;;             (,is1 (resolve-ix-index-specification
+;;                    (data-frame-column-index ,data-frame) ,is1)))
+;;        (declare (ignorable ,matrix ,column-index ,is0 ,is1))
+;;        ,@body)))
 
-(defmethod make-data-frame (matrix (keys sequence) &key copy?)
-  (make-data-frame matrix (make-hashed-index keys) :copy? copy?))
+;; (defmethod sub ((data-frame data-frame) &rest index-specifications)
+;;   (data-frame-with-resolved-index-specification 
+;;       (((matrix column-index) data-frame)
+;;        ((is0 is1) index-specifications))
+;;     (let ((sub-matrix (sub matrix is0 is1)))
+;;       (if (vectorp sub-matrix)
+;;           sub-matrix
+;;           (make-data-frame% sub-matrix (sub column-index is1))))))
 
-(defmethod dims ((data-frame data-frame))
-  (array-dimensions (data-frame-matrix data-frame)))
+;; (defmethod (setf sub) ((array array) (data-frame data-frame)
+;;                        &rest index-specifications)
+;;   (data-frame-with-resolved-index-specification
+;;       (((matrix column-index) data-frame)
+;;        ((is0 is1) index-specifications))
+;;     (setf (sub matrix is0 is1) array)))
 
-(defmethod element-type ((data-frame data-frame))
-  (array-element-type (data-frame-matrix data-frame)))
+;; (defmethod filter-rows (predicate (data-frame data-frame))
+;;   (let+ (((&structure data-frame- matrix column-index) data-frame))
+;;     (make-data-frame% (filter-rows predicate matrix) column-index)))
 
-(defmacro data-frame-with-resolved-index-specification
-    ((((matrix column-index) data-frame)
-      ((is0 is1) index-specifications))
-     &body body)
-  "Wrapper macro for resolving column index specifications."
-  (check-type matrix symbol)
-  (check-type column-index symbol)
-  (check-type is0 symbol)
-  (check-type is1 symbol)
-  (once-only (data-frame)
-    `(let+ (((&structure data-frame- (,matrix matrix)
-                         (,column-index column-index)) data-frame)
-            ((,is0 ,is1) ,index-specifications)
-            (,is1 (resolve-ix-index-specification
-                   (data-frame-column-index ,data-frame) ,is1)))
-       (declare (ignorable ,matrix ,column-index ,is0 ,is1))
-       ,@body)))
-
-(defmethod sub ((data-frame data-frame) &rest index-specifications)
-  (data-frame-with-resolved-index-specification 
-      (((matrix column-index) data-frame)
-       ((is0 is1) index-specifications))
-    (let ((sub-matrix (sub matrix is0 is1)))
-      (if (vectorp sub-matrix)
-          sub-matrix
-          (make-data-frame% sub-matrix (sub column-index is1))))))
-
-(defmethod (setf sub) ((array array) (data-frame data-frame)
-                       &rest index-specifications)
-  (data-frame-with-resolved-index-specification
-      (((matrix column-index) data-frame)
-       ((is0 is1) index-specifications))
-    (setf (sub matrix is0 is1) array)))
-
-(defmethod filter-rows (predicate (data-frame data-frame))
-  (let+ (((&structure data-frame- matrix column-index) data-frame))
-    (make-data-frame% (filter-rows predicate matrix) column-index)))
-
-(defmacro with-filter-data-frame (data-frame (&rest name-key-pairs) &body body)
-  "Similar to WITH-FILTER-ROWS, but also allows keys.  If the latter is not
-given, the quoted name will be used instead."
-  (with-unique-names (resolve index)
-    (let ((name-column-pairs
-           (mapcar (lambda (name-key-pair)
-                     (let+ (((name &optional (key `',name))
-                             (alexandria:ensure-list name-key-pair)))
-                       `(,name (,resolve ,key))))
-                   name-key-pairs)))
-      (once-only (data-frame)
-        `(let ((,index (data-frame-column-index ,data-frame)))
-           (flet ((,resolve (key)
-                    (if (typep key 'fixnum)
-                        key
-                        (ix ,index key))))
-             (with-filter-rows ,data-frame ,name-column-pairs
-               ,@body)))))))
+;; (defmacro with-filter-data-frame (data-frame (&rest name-key-pairs) &body body)
+;;   "Similar to WITH-FILTER-ROWS, but also allows keys.  If the latter is not
+;; given, the quoted name will be used instead."
+;;   (with-unique-names (resolve index)
+;;     (let ((name-column-pairs
+;;            (mapcar (lambda (name-key-pair)
+;;                      (let+ (((name &optional (key `',name))
+;;                              (alexandria:ensure-list name-key-pair)))
+;;                        `(,name (,resolve ,key))))
+;;                    name-key-pairs)))
+;;       (once-only (data-frame)
+;;         `(let ((,index (data-frame-column-index ,data-frame)))
+;;            (flet ((,resolve (key)
+;;                     (if (typep key 'fixnum)
+;;                         key
+;;                         (ix ,index key))))
+;;              (with-filter-rows ,data-frame ,name-column-pairs
+;;                ,@body)))))))
 
 ;; (defmethod map-columns (function (data-frame data-frame))
 ;;   (bind (((:structure data-frame- matrix column-index) data-frame)
@@ -98,9 +85,6 @@ given, the quoted name will be used instead."
 ;;     (if (vectorp result)
 ;;         result
 ;;         (make-data-frame% result column-index))))
-
-(defmethod as-array ((data-frame data-frame) &key copy?)
-  (maybe-copy-array (data-frame-matrix data-frame) copy?))
 
 ;; (defmethod shrink-rows ((data-frame data-frame) &key (predicate #'identity))
 ;;   (bind (((:structure data-frame- matrix column-index) data-frame)
