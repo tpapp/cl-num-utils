e4f30c053bebd0bcdd820e604256e06ac02d0357
tpapp <tkpapp@gmail.com>
2011-07-02 21:26:07 +0200

    Factored out PRETTY.

--- src/pretty.lisp
+++ src/pretty.lisp
@@ -28,24 +28,25 @@ single float."
 (defparameter *pretty-five-bias* 0.1d0
   "Default bias to 5's for PRETTY-STEP.")
 
-(defun pretty-step (width n &key 
-                    (min-step (default-min-step width))
-                    (bias *pretty-bias*) (five-bias *pretty-five-bias*))
-  "Return a `pretty' (meaning 1, 2, or 5*10^n) step size, and the
-number of fractional digits as the second value.  The algorithm is the
-following: WIDTH is divided to N intervals, and increased to MIN-STEP
-if necessary.  The logarithm is taken, to which BIAS is added.  The
-step size will be based on the fractional part.  FIVE-BIAS, also
-interpreted on a log scale, favors 5 over 2 as the first digit.  When
-BIAS is zero, STEP always divides WIDTH to at most N intervals."
-  (let+ ((raw-step (max (/ width (1+ n)) min-step))
-         ((&values exponent residual) (floor (+ (coerce (log raw-step 10)
-                                                        'double-float)
-                                                bias)))
+(defun pretty (x &key (bias *pretty-bias*) (five-bias *pretty-five-bias*))
+  "Return a rational that is close to x, and is a multiple of 1,2 or 5 times a
+power of 10.  The logarithm is taken, to which BIAS is added.  The result will
+be based on the fractional part.  FIVE-BIAS, also interpreted on a log scale,
+favors 5 over 2 as the first digit.  When BIAS favors larger values."
+  (let+ (((&values exponent residual)
+          (floor (+ (coerce (log x 10) 'double-float) bias)))
          (correction (cond
                        ((<= residual (- #.(log 2d0 10) five-bias)) 2)
                        ((<= residual #.(log 5d0 10)) 5)
                        (t 10))))
     (values (* correction (expt 10 exponent))
             (max 0 (- (if (= correction 10) -1 0) exponent)))))
+
+(defun pretty-step (width n &key 
+                    (min-step (default-min-step width))
+                    (bias *pretty-bias*) (five-bias *pretty-five-bias*))
+  "Return a `pretty' (meaning 1, 2, or 5*10^n) step size, and the number of
+fractional digits as the second value.  Uses PRETTY, but enforces a minimum.
+When BIAS is 0,, STEP always divides WIDTH to at most N intervals."
+  (pretty (max (/ width (1+ n)) min-step) :bias bias :five-bias five-bias))
 
