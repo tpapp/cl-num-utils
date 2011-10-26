e2a01b8bfa65e204ccd68bff1c70c5a374b78b32
Mason Smith <masonium@masonium.com>
2011-10-26 14:00:41 -0700

    Fixed issue 6 by computing proper type in e2+ and e2- specializations

--- src/elementwise.lisp
+++ src/elementwise.lisp
@@ -252,7 +252,9 @@ of arguments, no optional, key, rest etc)."
                      (row-major-aref b index))))
           result))
       (let* ((dimensions (common-dimensions a b))
-             (result (make-array dimensions :element-type t))
+             (result (make-array dimensions
+				 :element-type (emap-common-numeric-type
+						(array-element-type a) (array-element-type b))))
              (size (array-total-size result)))
         (assert dimensions)
         (dotimes (index size)
@@ -275,7 +277,9 @@ of arguments, no optional, key, rest etc)."
                      (row-major-aref b index))))
           result))
       (let* ((dimensions (common-dimensions a b))
-             (result (make-array dimensions :element-type t))
+             (result (make-array dimensions
+				 :element-type (emap-common-numeric-type
+						(array-element-type a) (array-element-type b))))
              (size (array-total-size result)))
         (assert dimensions)
         (dotimes (index size)
@@ -296,7 +300,9 @@ of arguments, no optional, key, rest etc)."
             (setf (row-major-aref result index)
                   (- a (row-major-aref b index))))
           result))
-      (let* ((result (make-array (array-dimensions b) :element-type t))
+      (let* ((result (make-array (array-dimensions b) :element-type
+				 (emap-common-numeric-type (type-of a)
+							   (array-element-type b))))
              (size (array-total-size result)))
         (dotimes (index size)
           (setf (row-major-aref result index) (- a (row-major-aref b index))))
