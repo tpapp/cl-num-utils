05392caf763ba5749d8ed4389aec41aaf781aff4
tpapp <tkpapp@gmail.com>
2011-10-29 19:23:22 +0200

    Fixed conflict.

--- tests/package.lisp
+++ tests/package.lisp
@@ -1,6 +1,6 @@
 (defpackage #:cl-num-utils-tests
   (:use cl alexandria iterate let-plus anaphora cl-num-utils lift)
-  (:shadowing-import-from cl-num-utils mean variance)
+  (:shadowing-import-from cl-num-utils mean variance median)
   (:export
 
    run-cl-num-utils-tests
