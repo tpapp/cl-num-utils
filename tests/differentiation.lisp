f7e8fb70fd7c2057b543eb40f68f9f678f6c1439
tpapp <tkpapp@gmail.com>
2011-11-21 12:30:12 +0100

    Added tests for constant elasticity.

--- tests/differentiation.lisp
+++ tests/differentiation.lisp
@@ -12,3 +12,11 @@
         (fp #'cos))
     (ensure-same (differentiate f 0d0) (funcall fp 0d0))
     (ensure-same (differentiate f 0.5d0) (funcall fp 0.5d0))))
+
+(addtest (differentiation-tests)
+  elasticity1
+  (let+ ((alpha 2d0)
+         ((&flet f (x) (expt x alpha)))
+         (elas (elasticity #'f)))
+    (ensure-same (funcall elas 2d0) alpha)
+    (ensure-same (funcall elas 7d0) alpha)))
