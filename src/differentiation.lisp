59686526e3f2a57662f0a0f6fa8f604edffea960
tpapp <tkpapp@gmail.com>
2012-10-11 13:49:32 +0200

    Added very basic numerical differentation for vector functions.

--- src/differentiation.lisp
+++ src/differentiation.lisp
@@ -11,16 +11,36 @@ elasticities)."))
 (defun differentiate (f x &key (n 1) (method :right) h)
   (differentiate% n method f x h))
 
-(defmethod differentiate% ((n (eql 1)) (method (eql :right)) f x h)
+(defun numdiff-epsilon (x &optional h)
+  "Sensible choice of epsilon for numerical differentiation."
+  (* (max (abs x) 1)
+     (if h
+         h
+         (sqrt double-float-epsilon))))
+
+(defmethod differentiate% ((n (eql 1)) (method (eql :right)) f (x real) h)
   (let* ((x (float x 1d0))
-         (h (* (max (abs x) 1)
-               (if h
-                   h
-                   (sqrt double-float-epsilon))))
+         (h (numdiff-epsilon x h))
          (fx (funcall f x)))
     (values (/ (- (funcall f (+ x h)) fx)
                h)
             fx)))
+
+(defun add-standard-basis-vector (x axis h)
+  "Return a x+e, where e_i = h if i=axis, 0 otherwise."
+  (aprog1 (copy-array x)
+    (incf (aref it axis) h)))
+
+(defmethod differentiate% ((n (eql 1)) (method (eql :right)) f (x vector) h)
+  (let ((fx (funcall f x))
+        (h (map 'vector (lambda (x) (numdiff-epsilon x h)) x))
+        (length (length x)))
+    (aprog1 (make-array length)
+      (loop for axis below length
+            for h across h
+            do (setf (aref it axis)
+                     (/ (- (funcall f (add-standard-basis-vector x axis h)) fx)
+                        h))))))
 
 ;;; !!! todo: write two-sided, left, Richardson approximation, etc
 
--- tests/differentiation.lisp
+++ tests/differentiation.lisp
@@ -20,3 +20,9 @@
          (elas (elasticity #'f)))
     (ensure-same (funcall elas 2d0) alpha)
     (ensure-same (funcall elas 7d0) alpha)))
+
+(addtest (differentiation-tests)
+  differentiation2
+  (ensure-same
+   (differentiate (lambda (x) (let+ ((#(x0 x1) x)) (+ (expt x0 2) (* 3 x1)))) #(0 0))
+   #(0 3)))
