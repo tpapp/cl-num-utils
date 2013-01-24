ce1d4fe71d736e7cdb9d1fb2c46ec8998e46c8f5
tpapp <tkpapp@gmail.com>
2013-01-24 14:13:31 +0100

    Re-added some convenience macros.

--- src/old/macros.lisp
+++ src/old/macros.lisp
@@ -121,27 +121,3 @@ All variables are declared DOUBLE-FLOAT in the body."
     `(let ,bindings
        (declare (type double-float ,@(mapcar #'first bindings)))
        ,@body)))
-
-(defmacro gethash* (key hash-table
-                    &optional (datum "Key not found.")
-                    &rest arguments)
-  "Like GETHASH, but checking that KEY is present and raising the given
-error if not."
-  (with-unique-names (value present?)
-    `(multiple-value-bind (,value ,present?) (gethash ,key ,hash-table)
-       (assert ,present? () ,datum ,@arguments)
-       ,value)))
-
-(defmacro splice-when (test &body forms)
-  "Similar to when, but wraps the result in list.
-
-Example: `(,foo ,@(splice-when add-bar? bar))"
-  `(when ,test
-     (list
-      (progn ,@forms))))
-
-(defmacro splice-awhen (test &body forms)
-  "Similar to splice-when, but binds IT to test."
-  `(awhen ,test
-     (list
-      (progn ,@forms))))
--- src/utilities.lisp
+++ src/utilities.lisp
@@ -1,2 +1,34 @@
-(defpackage #:cl-num-utils.utilities
-  (:use #:cl))
+;;; -*- Mode:Lisp; Syntax:ANSI-Common-Lisp; -*-
+
+(cl:defpackage #:cl-num-utils.utilities
+  (:use #:cl #:alexandria)
+  (:export
+   #:gethash*
+   #:splice-when
+   #:splice-awhen))
+
+(cl:in-package #:cl-num-utils.utilities)
+
+(defmacro gethash* (key hash-table
+                    &optional (datum "Key not found.")
+                    &rest arguments)
+  "Like GETHASH, but checking that KEY is present and raising the given
+error if not."
+  (with-unique-names (value present?)
+    `(multiple-value-bind (,value ,present?) (gethash ,key ,hash-table)
+       (assert ,present? () ,datum ,@arguments)
+       ,value)))
+
+(defmacro splice-when (test &body forms)
+  "Similar to when, but wraps the result in list.
+
+Example: `(,foo ,@(splice-when add-bar? bar))"
+  `(when ,test
+     (list
+      (progn ,@forms))))
+
+(defmacro splice-awhen (test &body forms)
+  "Similar to splice-when, but binds IT to test."
+  `(awhen ,test
+     (list
+      (progn ,@forms))))
